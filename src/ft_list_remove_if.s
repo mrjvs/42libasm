a918347f391ab8105e14472b985b1e5117da1fb3
Jelle Van snik <jvan-sni@f0r4s25.codam.nl>
2020-01-07 18:17:49 +0100

    Finished ft_list_remove_if

--- src/ft_list_remove_if.s
+++ src/ft_list_remove_if.s
@@ -1,32 +1,60 @@
 section .text
 	global	_ft_list_remove_if		; set function as global
-	extern	_malloc
+	extern	_free
 
 _ft_list_remove_if:
 	enter 0, 0
-	cmp rdi, 0					; check if NULL pointer, return if it is
+	cmp rdi, 0					; check if list is NULL pointer, return if it is
 	je .return
-	mov rdi, QWORD [rdi]
-	mov r10, rdx
+	mov r10, QWORD rdi			; save parent pointer to reg 10
+	mov rdi, QWORD [rdi]		; derefrence list once
+	cmp rdi, 0					; check if list item is NULL pointer, return if it is
+	je .return
 .loop:
 
-	mov r11, QWORD [rdi]
+	mov r11, QWORD [rdi]		; move data of current list item to reg 11
 	push rdi					; setup stack for function call
 	push rsi
-	push r10
 	push rdx
 
-	mov rdi, rsi
+	mov rdi, rsi				; setup args for function call
 	mov rsi, r11
-	call r10					; call compare function
+	call rdx					; call compare function
 
-	pop rdx
-	pop r10
-	pop rsi						; clean stack from function call
+	pop rdx						; clean stack from function call
+	pop rsi
 	pop rdi
 
+	cmp rax, 0					; check compare return value
+	jne .normal					; skip stitching if difference
+
+	mov r9, [rdi+8]
+	mov QWORD [r10], r9			; stitch list->next to parent pointer
+
+	push rdi					; setup stack for function call
+	push rsi
+	push rdx
+	push r10
+
+	mov rdi, r11				; move list data to function arg
+	call _free					; call free
+
+	pop r10
+	pop rdx						; clean stack from function call
+	pop rsi
+	pop rdi
+
+	jmp .afterstitch
+
+.normal:
+	mov r10, rdi				; save parent pointer of next list item
+	add r10, 8
+
+.afterstitch:
 	cmp QWORD [rdi+8], 0		; check if list->next is NULL pointer, return if it is
 	je .return
+
+
 	mov rdi, QWORD [rdi+8]		; set rdi to next list item
 	jmp .loop					; jump to start of loop
 .return:
--- test/test_ft_list_remove_if.c
+++ test/test_ft_list_remove_if.c
@@ -6,7 +6,7 @@
 /*   By: jvan-sni <jvan-sni@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/06 14:27:07 by jvan-sni       #+#    #+#                */
-/*   Updated: 2020/01/06 18:21:40 by jvan-sni      ########   odam.nl         */
+/*   Updated: 2020/01/07 17:55:04 by jvan-sni      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,7 @@
 
 static int	compare(char *s1, char *s2)
 {
-	(void)s1;
-	(void)s2;
-	printf("ran compare on '%s' and '%s'\n", s1, s2);
-	return (1);
+	return (strcmp(s1, s2));
 }
 
 static void	print_ft_list(t_list *lst)
@@ -43,14 +40,58 @@ void		test_ft_list_remove_if(void)
 	t_list	*lst;
 
 	list1.next = &list2;
-	list1.data = "yes";
+	list1.data = strdup("no");
 	list2.next = &list3;
-	list2.data = "no";
+	list2.data = strdup("no");
 	list3.next = NULL;
-	list3.data = "yes";
+	list3.data = strdup("123");
+	printf("PTR %p\n", list1.data);
 	lst = &list1;
-	printf("%p\n", list1.data);
 	print_ft_list(lst);
+	puts("rm 'no'");
 	ft_list_remove_if(&lst, "no", compare);
 	print_ft_list(lst);
+	puts("---");
+	list1.next = &list2;
+	list1.data = strdup("yes");
+	list2.next = &list3;
+	list2.data = strdup("no");
+	list3.next = NULL;
+	list3.data = strdup("no");
+	lst = &list1;
+	print_ft_list(lst);
+	puts("rm 'no'");
+	ft_list_remove_if(&lst, "no", compare);
+	print_ft_list(lst);
+	puts("---");
+	list1.next = &list2;
+	list1.data = strdup("yes");
+	list2.next = &list3;
+	list2.data = strdup("no");
+	list3.next = NULL;
+	list3.data = strdup("yes");
+	lst = &list1;
+	print_ft_list(lst);
+	puts("rm 'yes'");
+	ft_list_remove_if(&lst, "yes", compare);
+	print_ft_list(lst);
+	puts("---");
+	list1.next = &list2;
+	list1.data = strdup("yes");
+	list2.next = &list3;
+	list2.data = strdup("yes");
+	list3.next = NULL;
+	list3.data = strdup("yes");
+	lst = &list1;
+	print_ft_list(lst);
+	puts("rm 'yes'");
+	ft_list_remove_if(&lst, "yes", compare);
+	print_ft_list(lst);
+	puts("---");
+	lst = NULL;
+	print_ft_list(lst);
+	puts("rm 'yes'");
+	ft_list_remove_if(&lst, "yes", compare);
+	print_ft_list(lst);
+	ft_list_remove_if(NULL, "yes", compare);
 }
